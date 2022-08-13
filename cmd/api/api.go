2f61bb92cbb948bb25941b96fc4b208fc873198c
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-08-13 14:31:24 +0700

    cleaning API URL & Stripe Key; add driver func to connect DB

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"web-app-stripe/internal/driver"
 )
 
 const version = "1.0.0"
@@ -50,6 +51,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4001, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "dev", "Application environment {development|production|maintenance}")
+	flag.StringVar(&cfg.db.dsn, "dsn", "DSN", "DSN")
 
 	flag.Parse()
 
@@ -59,6 +61,12 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	conn, err := driver.OpenDB(cfg.db.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer conn.Close()
+
 	app := &application{
 		config:   cfg,
 		infoLog:  infoLog,
@@ -66,7 +74,7 @@ func main() {
 		version:  version,
 	}
 
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
 		app.errorLog.Println(err)
 		log.Fatal(err)
