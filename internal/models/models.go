410d5233209e5d35353b56b2221163e6696b992e
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-08-15 11:26:55 +0700

    add query func insert-transaction

--- internal/models/models.go
+++ internal/models/models.go
@@ -85,7 +85,7 @@ type User struct {
 	UpdatedAt time.Time `json:"-"`
 }
 
-func (m DBModel) GetWidget(id int) (Widget, error) {
+func (m *DBModel) GetWidget(id int) (Widget, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
@@ -115,4 +115,39 @@ func (m DBModel) GetWidget(id int) (Widget, error) {
 	}
 
 	return widget, nil
+}
+
+// InsertTransaction insert a new transaction and returns its id
+func (m *DBModel) InsertTransaction(txn Transaction) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `
+		insert 
+			into transactions
+			(amount, currency, last_four, bank_return_code, transaction_status_id, created_at, updated_at)
+		values
+			(?, ?, ?, ?, ?, ?, ?)
+	`
+
+	result, err := m.DB.ExecContext(ctx, stmt,
+		txn.Amount,
+		txn.Currency,
+		txn.LastFour,
+		txn.BankReturnCode,
+		txn.TransactionStatusID,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
