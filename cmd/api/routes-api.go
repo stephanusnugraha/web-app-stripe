1872a38ace39e6ba2248c04ba35d80a9bbdf018e
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-08-23 17:15:59 +0700

    add handler for bronze plan (create cust & subs)

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -19,5 +19,6 @@ func (app *application) routes() http.Handler {
 
 	mux.Post("/api/payment-intent", app.GetPaymentIntent)
 	mux.Get("/api/widget/{id}", app.GetWidgetByID)
+	mux.Post("/api/create-customer-and-subscribe-to-plan", app.CreateCustomerAndSubscribeToPlan)
 	return mux
 }
