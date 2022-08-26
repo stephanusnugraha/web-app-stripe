2fad565c0679e2fdde0583a487b6e9b8aa7e6750
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-08-26 11:44:15 +0700

    create query db for get user by email

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -262,6 +262,18 @@ func (app *application) CreateAuthToken(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
+	// get the user from the database by email; send error if invalid email
+	user, err := app.DB.GetUserByEmail(userInput.Email)
+	if err != nil {
+
+	}
+
+	// validate the password; send error if invalid password
+
+	// generate the token
+
+	// send response
+
 	var payload struct {
 		Error   bool   `json:"error"`
 		Message string `json:"message"`
--- internal/models/models.go
+++ internal/models/models.go
@@ -3,6 +3,7 @@ package models
 import (
 	"context"
 	"database/sql"
+	"strings"
 	"time"
 )
 
@@ -84,6 +85,7 @@ type Transaction struct {
 
 // User is the type for users
 type User struct {
+	ID        int       `json:"id"`
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
 	Email     string    `json:"email"`
@@ -243,4 +245,37 @@ func (m *DBModel) InsertCustomer(c Customer) (int, error) {
 	}
 
 	return int(id), nil
+}
+
+// GetUserByEmail gets a user by email address
+func (m *DBModel) GetUserByEmail(email string) (User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	email = strings.ToLower(email)
+	var u User
+
+	querySQL := `
+		select 
+			id, first_name, last_name, email, password, created_at, updated_at
+		from
+			users
+		where email = ?`
+
+	row := m.DB.QueryRowContext(ctx, querySQL, email)
+
+	err := row.Scan(
+		&u.ID,
+		&u.FirstName,
+		&u.LastName,
+		&u.Email,
+		&u.Password,
+		&u.CreatedAt,
+		&u.UpdatedAt,
+	)
+	if err != nil {
+		return u, err
+	}
+
+	return u, nil
 }
