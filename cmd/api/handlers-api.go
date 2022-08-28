523f88260369b608ff2f98e40179cf92d48fbf39
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-08-28 14:43:49 +0700

    add func to get user for token from db

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -312,8 +312,6 @@ func (app *application) CreateAuthToken(w http.ResponseWriter, r *http.Request) 
 }
 
 func (app *application) authenticateToken(r *http.Request) (*models.User, error) {
-	var u models.User
-
 	authorizationHeader := r.Header.Get("Authorization")
 	if authorizationHeader == "" {
 		return nil, errors.New("no authorization header received")
@@ -330,8 +328,12 @@ func (app *application) authenticateToken(r *http.Request) (*models.User, error)
 	}
 
 	// get the user from the tokens table
+	user, err := app.DB.GetUserForToken(token)
+	if err != nil {
+		return nil, errors.New("no matching user found")
+	}
 
-	return &u, nil
+	return user, nil
 }
 
 func (app *application) CheckAuthentication(w http.ResponseWriter, r *http.Request) {
