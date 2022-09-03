4ec4915722992f7265f73c12a3bb6e80a19e2f1f
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-09-03 15:00:40 +0700

    create logout func for front-end to destroy token

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -332,3 +332,19 @@ func (app *application) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 	app.Session.Put(r.Context(), "userID", id)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
+	err := app.Session.Destroy(r.Context())
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	err = app.Session.RenewToken(r.Context())
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
