87afb2a781719e4017dcd8182d0b7011984ce51f
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-09-04 13:06:33 +0700

    create mail template and add handlers to send email

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -436,4 +436,19 @@ func (app *application) SendPasswordResetEmail(w http.ResponseWriter, r *http.Re
 	data.Link = "http://www.unb.ca"
 
 	//send mail
+	err = app.SendMail("info@widgets.com", "info@widgets.com", "Password Reset Request", "password-reset", data)
+	if err != nil {
+		app.errorLog.Println(err)
+		app.badRequest(w, r, err)
+		return
+	}
+
+	var resp struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+
+	resp.Error = false
+
+	app.writeJSON(w, http.StatusCreated, resp)
 }
