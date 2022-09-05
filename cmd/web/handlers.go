b02ad10330d0dad170ff70f8c94c319c8540f1e9
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-09-05 13:49:46 +0700

    config the reset-password page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -367,9 +367,17 @@ func (app *application) ShowResetPassword(w http.ResponseWriter, r *http.Request
 
 	valid := signer.VerifyToken(testURL)
 
-	if valid {
-		w.Write([]byte("valid"))
-	} else {
-		w.Write([]byte("invalid"))
+	if !valid {
+		app.errorLog.Println("Invalid URL - tempering detected")
+		return
+	}
+
+	data := make(map[string]interface{})
+	data["email"] = r.URL.Query().Get("email")
+
+	if err := app.renderTemplate(w, r, "reset-password", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
 	}
 }
