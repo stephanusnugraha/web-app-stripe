2a15becfb30db4556a3f59f6729ef60a7628c7de
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-09-12 09:48:05 +0700

    add func cancel subscription

--- internal/card/cards/cards.go
+++ internal/card/cards/cards.go
@@ -137,6 +137,21 @@ func (c *Card) Refund(pi string, amount int) error {
 	return nil
 }
 
+func (c *Card) CancelSubscription(subID string) error {
+	stripe.Key = c.Secret
+
+	params := &stripe.SubscriptionParams{
+		CancelAtPeriodEnd: stripe.Bool(true),
+	}
+
+	_, err := sub.Update(subID, params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func cardErrorMessage(code stripe.ErrorCode) string {
 	var msg = ""
 	switch code {
