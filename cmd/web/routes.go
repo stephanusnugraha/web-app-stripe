1f4e23ceecbdcde9961bef0aaec4cf3864f78cce
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-09-18 11:36:30 +0700

    add routes and handlers for all-users and one-user

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,8 @@ func (app *application) routes() http.Handler {
 		mux.Get("/all-subscriptions", app.AllSubscriptions)
 		mux.Get("/sales/{id}", app.ShowSale)
 		mux.Get("/subscriptions/{id}", app.ShowSubscription)
+		mux.Get("/all-users", app.AllUsers)
+		mux.Get("/all-users/{id}", app.OneUser)
 	})
 
 	mux.Get("/receipt", app.Receipt)
