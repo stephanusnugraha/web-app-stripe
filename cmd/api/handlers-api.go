0f4875b14cb349f70308f65a56e2af8784424fc5
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-09-22 09:46:30 +0700

    add function to delete user

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -788,3 +788,25 @@ func (app *application) EditUser(w http.ResponseWriter, r *http.Request) {
 	resp.Error = false
 	app.writeJSON(w, http.StatusOK, resp)
 }
+
+func (app *application) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	userID, err := strconv.Atoi(id)
+	if err != nil {
+		app.badRequest(w, r, err)
+	}
+
+	err = app.DB.DeleteUser(userID)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	var resp struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+
+	resp.Error = false
+	app.writeJSON(w, http.StatusOK, resp)
+}
